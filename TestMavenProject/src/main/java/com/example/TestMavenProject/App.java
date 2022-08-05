3d3e45285942d759617a6fb69346985146afd211
JPEG1 <jmo442002@gmail.com>
2022-08-06 01:02:05 +0530

    Adding Maven Project

---
+++ TestMavenProject/src/main/java/com/example/TestMavenProject/App.java
@@ -0,0 +1,17 @@
+package com.example.TestMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int num1, num2, result;
+    	num1 = 50;
+    	num2 = 70;
+    	result = num1 + num2;
+        System.out.println( "Sum: " + result );
+    }
+}
